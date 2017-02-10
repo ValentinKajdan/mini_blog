56480b4f7ce60cd3621a46cdcc5aedf0b11b657a
ValentinKajdan <valentin.kajdan@orange.fr>
2017-02-10 19:07:34 +0100

    show categories

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,45 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('/home.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+    * @Route("/categories", name="categories_list")
+    */
+   public function categoriesListAction(Request $request)
+   {
+       // $categories = $this
+       //     ->getDoctrine()
+       //     ->getRepository('AppBundle:Category')
+       //     ->findAll()
+       // ;
+       $categories = $this
+           ->getDoctrine()
+           ->getRepository('AppBundle:Category')
+           ->findBy([])
+       ;
+       return $this->render('categories/categories_list.html.twig', [
+           'categories' => $categories
+       ]);
+   }
+
+   /**
+     * @Route("/categories/{id}", name="categories_name")
+     */
+    public function categoriesDetailsAction(Request $request, $id)
+    {
+        $category = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:Category')
+            ->getById($id)
+        ;
+        return $this->render('categories/categories_details.html.twig', [
+          'category' => $category
+        ]);
+    }
+
+
 }
