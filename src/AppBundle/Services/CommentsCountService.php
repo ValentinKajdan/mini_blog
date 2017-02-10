1b8074dd63ae4be29f3d791b7d016275c45196a6
ValentinKajdan <valentin.kajdan@orange.fr>
2017-02-11 00:46:13 +0100

    init service twig counter

---
+++ src/AppBundle/Services/CommentsCountService.php
@@ -0,0 +1,40 @@
+<?php
+namespace AppBundle\Services;
+use Doctrine\ORM\EntityManager;
+class CommentsCounterService
+{
+    /**
+     * @var EntityManager
+     */
+    private $doctrine;
+    public function __construct($doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+    /**
+     * Retourne le nombre total de notes sur un menu
+     * @param  int $menuId Identifiant du menu
+     * @return int
+     */
+    public function getTotalMenuLike($menuId)
+    {
+        return $this
+            ->doctrine
+            ->getRepository('AppBundle:MenuLike')
+            ->getCountMenuLike($menuId)
+        ;
+    }
+    /**
+     * Retourne la moyenne des notes sur un menu
+     * @param  int $menuId Identifiant du menu
+     * @return int
+     */
+    public function getAvgMenuLike($menuId)
+    {
+        return $this
+            ->doctrine
+            ->getRepository('AppBundle:MenuLike')
+            ->getAvgMenuLike($menuId)
+        ;
+    }
+}
